649e39f89fb59c379165d895a0f35873111e05bf
unknown <mousazadealireza8@gmail.com>
2026-08-05 21:42:57 +0430

    add: encoder

--- rs.py
+++ rs.py
@@ -2,9 +2,32 @@
 from gf import *
 import cir, poly
 def encode(n, k, t, /, u:list, gen:list, field):
-	pass
+	''' Encode non-binary bch code with length 'n'.
+	
+	Key Arguments:
+	n --
+	k --
+	t -- 
+	u -- information src
+	gen -- generator polynomial
+	field -- q-ary field'''
+	alph, loga =field[0], field[1]
+	regs =[0]*(len(gen) -1)
+	for clk in range(len(u) + len(gen) - 1):
+		regs[0] = u[-(clk + 1)]
+
+		for i in range(len(regs)):
+			if (regs[-1] ==0):
+				regs[i] = regs[i-1]
+			else:
+				regs[i] ^= mul(regs[i-1], gen[i], field)
+
+	return regs
+
 def encode_textbook(n, k, t, /, u :list, gen :list, field):
-	''' Encode non-binary bch code with length 'n'.'''
+	''' Encode non-binary bch code with length 'n'.
+	
+	Notes: this is textbook encoder which is a systematic encoder (uses long devision).'''
 	alph =field[0]
 	loga =field[1]
 	codeword = [0]*(n-k) + u
@@ -23,7 +46,7 @@ def encode_textbook(n, k, t, /, u :list, gen :list, field):
 		  		gen(leadg),
 				field)
 		pass
-		
+	pass
 
 def syndrome(n, k, t, /, r :list, field) -> tuple:
 	"""Calculate non-binary bch syndrome."""
@@ -45,7 +68,7 @@ def syndrome(n, k, t, /, r :list, field) -> tuple:
 		
 	return S
 
-f = exfield_gen(4, 0b11001)
+f16 = exfield_gen(4, 0b11001)
 # b = [f[0][0], f[0][5], f[0][10]]
 
 # print(syndrome(15, 9, 2, r =[b[2], 0, b[1], b[2], 0, b[1], b[0]
